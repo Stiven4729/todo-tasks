2ca44066b04e22ad370a333a8f49edcf3c1ea154
Stiven Gallego <gallegostiven4729@gmail.com>
2023-08-14 21:07:50 -0500

    Variables de entorno, modelo, api, crear tarea

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
+const api = require('./api');
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.listen(port, function () {
     console.log("Server is listening at port: " + port);
@@ -9,3 +13,5 @@ app.listen(port, function () {
 app.get('/', function (req, res) {
     res.send("Stiven Gallego");
 });
+    
+app.use('/api', api);
